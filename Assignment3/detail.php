809b6a3ed552bdef8e7ac26474198938d55159b2
Logan Watts <Loganary1@gmail.com>
2024-09-17 17:54:23 -0400

    added code to detail page

--- Assignment3/detail.php
+++ Assignment3/detail.php
@@ -1,3 +1,49 @@
 <?php
+    $i = $_GET['post_id'];
 
+    $blog_posts=[
+        [
+            'title'=>'First php Blog',
+            'content'=>'This is my first time creating a php blog. It\'s pretty cool!',
+            'author'=>'Emery Frondorf',
+            'date'=>'9/16/2024'
+        ],
+        [
+            'title'=>'Second php Blog',
+            'content'=>'This is my first time creating a php blog. It\'s pretty cool!',
+            'author'=>'Logan Watts',
+            'date'=>'9/16/2024'
+        ],
+        [
+            'title'=>'Third php Blog',
+            'content'=>'This is my first time creating a php blog. It\'s pretty cool!',
+            'author'=>'David-Michael Davies',
+            'date'=>'9/16/2024'
+        ]
+    ];
+
+
+    function displayPost($post)
+    {
+        /*foreach($post as $line)
+        {
+            echo $line.'<br/>';
+        }*/
+        echo '<h1>'.$post['title'].'</h1>';
+        echo '<h4>Authored by '.$post['author'].' on '.$post['date'].'</h4>';
+        echo '<p>'.$post['content'].'</p>';
+    }
+    
 ?>
+
+<html>
+    <title><?=$blog_posts[$i]['title']?></title>
+    <body>
+        <div>
+        <?php
+            displayPost($blog_posts[$i]);
+        ?>
+        </div>
+
+    </body>
+</html>
